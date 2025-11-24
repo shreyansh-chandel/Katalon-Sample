99ffbc94eeb2ead64a2d64406618c25117fc25a1
shreyanshc <shreyanshc@lambdatest.com>
2025-11-24 20:34:15 +0530

    added test suite

---
+++ Test Suites/Sample Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sample Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b01b39b2-5a81-48d6-92ba-0a1b7da04a29</testSuiteGuid>
+</TestSuiteEntity>
