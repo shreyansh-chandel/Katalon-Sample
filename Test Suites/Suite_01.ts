7287745980c722ed03a6db70b9b4b996f8444421
shreyanshc <shreyanshc@lambdatest.com>
2025-11-24 20:54:07 +0530

    Renamed Suite_01

--- Test Suites/Suite_01.ts
+++ Test Suites/Suite_01.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Sample Suite</name>
+   <name>Suite_01</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>b01b39b2-5a81-48d6-92ba-0a1b7da04a29</testSuiteGuid>
+   <testCaseLink>
+      <guid>12b09385-b80f-427b-90a8-aeb0c885d75a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
