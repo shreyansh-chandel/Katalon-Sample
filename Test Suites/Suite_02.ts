b237e8163033e2cc40edd3263ed3b8f0d924b947
shreyanshc <shreyanshc@lambdatest.com>
2025-11-25 13:45:02 +0530

    added test case two

---
+++ Test Suites/Suite_02.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite_02</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>39de0847-e98a-4eaa-8108-a896d54e6007</testSuiteGuid>
+</TestSuiteEntity>
